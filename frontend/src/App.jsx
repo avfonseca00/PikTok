09c3c895de6e28d5f58ab0b3670023e74052a2cb
avfonseca00 <avfonseca00@gmail.com>
2025-02-03 20:12:46 -0500

    Changed js to jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import VideoPlayer from './components/VideoPlayer';
+
+
+const App = () => {
+  return (
+    <main>
+      <VideoPlayer/>
+    </main>
+  )
+}
+
+export default App;
